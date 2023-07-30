b74f13d09caed3f82539caa5ed8a01549fdc43b9
Ishan Pathak <pathakishan333@gmail.com>
2023-07-30 15:35:23 -0500

    Added You Won text

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ function App() {
     <main>
       {tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
-      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      {tenzies ? <p className="instructions">YOU WON!!!</p> :<p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>}
       <div className="dice-container">
         {dieElement}
       </div>
